34836957663dbb0e8ef8f6d635ab3ae038141850
Karen Huaulme <karen.huaulme@gmail.com>
2022-07-05 16:37:45 +0200

    advanced scouting

--- src/App.js
+++ src/App.js
@@ -113,26 +113,18 @@ const App = () => {
         Atlas Search Soccer
       </h2>
       <div className="flex mx-20 w-full justify-evenly items-center">
-        <img
-          src={SpotSearch}
-          alt="advanced search"
-          className="rounded-lg w-40 object-contain"
-          onClick={() => setShowAdvancedSearch(!showAdvancedSearch)}
-        ></img>
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-10 w-10 text-white mb-12"
-          viewBox="0 0 20 20"
-          fill="currentColor"
+        <div
+          className=" text-lg text-mongo-400 font-bold"
           onClick={() => setShowAdvancedSearch(!showAdvancedSearch)}
         >
-          <path d="M9 9a2 2 0 114 0 2 2 0 01-4 0z" />
-          <path
-            fillRule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a4 4 0 00-3.446 6.032l-2.261 2.26a1 1 0 101.414 1.415l2.261-2.261A4 4 0 1011 5z"
-            clipRule="evenodd"
-          />
-        </svg> */}
+          <img
+            src={SpotSearch}
+            alt="advanced search"
+            className="rounded-lg w-48 object-contain mb-4"
+          ></img>
+          {" Advanced Scouting "}
+        </div>
+
         <SearchBar
           searchTerm={searchTerm}
           setSearchTerm={setSearchTerm}
