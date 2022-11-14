cfd78ee6ef0bc79baf728e4aab7812cb30c5e7b2
Karen Huaulme <karen.huaulme@gmail.com>
2022-11-14 14:54:22 -0500

    scrolling in like players section

--- src/components/LikePlayersSection.js
+++ src/components/LikePlayersSection.js
@@ -3,11 +3,14 @@ import LikePlayerCard from "./LikePlayerCard";
 
 const LikePlayersSection = ({ players }) => {
   return (
-    <div className="flex flex-col w-1/4  bg-gray-900 items-center ">
-      <div className="font-bold text-2xl text-green-500 my-4">
+    <div className="flex flex-col w-1/4  bg-gray-900 items-center overflow-auto ">
+      <div className="font-bold text-2xl text-green-500 mt-4">
         More Players Like This
       </div>
-      <div className="flex flex-col justify-center divide-y divide-solid">
+      <div className=" text-lg text-yellow-500 my-2">
+        based on traits and position
+      </div>
+      <div className="flex flex-col justify-center divide-y divide-solid border-t border-white">
         {players?.map((player, index) => (
           <LikePlayerCard key={index} player={player} />
         ))}
